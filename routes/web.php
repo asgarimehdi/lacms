2058f96409ac0f4efa16bb5bd53534850eb54574
asgarimehdi <mehdi.asgari@gmail.com>
2026-07-09 19:42:23 +0330

    feat: implement auth flow and admin user

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,21 @@
 <?php
 
-use App\Http\Livewire\PageManager;
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Auth\Login;
+use App\Http\Livewire\PageManager;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', fn () => redirect()->route('login'));
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', Login::class)->name('login');
 });
 
-Route::get('/admin/pages', PageManager::class)->name('pages.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/admin/pages', PageManager::class)->name('pages.index');
+});
+
+Route::post('/logout', function () {
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect()->route('login');
+})->name('logout');
