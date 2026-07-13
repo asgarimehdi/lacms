4c5c1b5c7063403abcbdb764cb83daf3a18d33a8
Mehdi Asgari <47991557+asgarimehdi@users.noreply.github.com>
2026-07-13 13:03:22 +0330

    اصلاح نمایش Editor.js - حذف x-show برای نمایش صحیح

--- resources/views/pages/posts/create-edit.blade.php
+++ resources/views/pages/posts/create-edit.blade.php
@@ -78,13 +78,10 @@ new class extends Component {
             <x-select label="{{ __('cms.category') }}" wire:model="category_id" :options="$categories" placeholder="{{ __('cms.select_category') }}" />
             <x-choices-offline label="Tags" wire:model="tag_ids" :options="$tags" searchable />
 
-            <div class="mt-4" x-data="{ ready: false }" x-init="ready = true">
+            <div class="mt-4">
                 <label class="label"><span class="label-text">{{ __('cms.content') }}</span></label>
                 <input type="hidden" wire:model="content" id="post-content" />
-                <div x-show="ready" data-editorjs="post-content" class="min-h-[400px] border border-base-300 rounded-lg p-4 prose prose-sm max-w-none bg-base-100"></div>
-                <div x-show="!ready" class="min-h-[400px] border border-base-300 rounded-lg bg-base-300 animate-pulse flex items-center justify-center">
-                    <x-icon name="o-cube" class="w-12 h-12 text-base-content/20" />
-                </div>
+                <div data-editorjs="post-content" class="min-h-[400px] border border-base-300 rounded-lg p-4 prose prose-sm max-w-none bg-base-100"></div>
             </div>
 
             <x-checkbox label="{{ __('cms.published') }}" wire:model="is_published" class="mt-4" />
