f54d62726deafd318b902854c266a980819b348c
Mehdi Asgari <47991557+asgarimehdi@users.noreply.github.com>
2026-07-13 12:34:06 +0330

    نمایش منوی سایدبار فقط برای کاربران لاگین شده

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,45 +25,47 @@
     {{-- MAIN --}}
     <x-main>
         {{-- SIDEBAR --}}
-        <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
+        @auth
+            <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
 
-            {{-- BRAND --}}
-            <x-app-brand class="px-5 pt-4" />
+                {{-- BRAND --}}
+                <x-app-brand class="px-5 pt-4" />
 
-            {{-- MENU --}}
-            <x-menu activate-by-route>
+                {{-- MENU --}}
+                <x-menu activate-by-route>
 
-                {{-- User --}}
-                @if($user = auth()->user())
+                    {{-- User --}}
+                    @if($user = auth()->user())
+                        <x-menu-separator />
+
+                        <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
+                            <x-slot:actions>
+                                <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
+                            </x-slot:actions>
+                        </x-list-item>
+
+                        <x-menu-separator />
+                    @endif
+
+                    {{-- CMS Menu --}}
+                    <x-menu-item title="{{ __('cms.dashboard') }}" icon="o-home" link="/admin" />
+                    <x-menu-item title="{{ __('cms.posts') }}" icon="o-document" link="/admin/posts" />
+                    <x-menu-item title="{{ __('cms.pages') }}" icon="o-document-text" link="/admin/pages" />
+                    <x-menu-item title="{{ __('cms.categories') }}" icon="o-folder" link="/admin/categories" />
+                    <x-menu-item title="Tags" icon="o-tag" link="/admin/tags" />
+                    <x-menu-item title="Settings" icon="o-cog-6-tooth" link="/admin/settings" />
                     <x-menu-separator />
 
-                    <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
-                        <x-slot:actions>
-                            <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
-                        </x-slot:actions>
-                    </x-list-item>
+                    {{-- Theme Toggle --}}
+                    <div class="px-5 py-2 flex items-center justify-between">
+                        <span class="text-xs uppercase opacity-60">Theme</span>
+                        <x-theme-toggle />
+                    </div>
 
-                    <x-menu-separator />
-                @endif
-
-                {{-- CMS Menu --}}
-                <x-menu-item title="{{ __('cms.dashboard') }}" icon="o-home" link="/admin" />
-                <x-menu-item title="{{ __('cms.posts') }}" icon="o-document" link="/admin/posts" />
-                <x-menu-item title="{{ __('cms.pages') }}" icon="o-document-text" link="/admin/pages" />
-                <x-menu-item title="{{ __('cms.categories') }}" icon="o-folder" link="/admin/categories" />
-                <x-menu-item title="Tags" icon="o-tag" link="/admin/tags" />
-                <x-menu-item title="Settings" icon="o-cog-6-tooth" link="/admin/settings" />
-                <x-menu-separator />
-
-                {{-- Theme Toggle --}}
-                <div class="px-5 py-2 flex items-center justify-between">
-                    <span class="text-xs uppercase opacity-60">Theme</span>
-                    <x-theme-toggle />
-                </div>
-
-                <x-menu-item title="Public Site" icon="o-globe-alt" link="/" />
-            </x-menu>
-        </x-slot:sidebar>
+                    <x-menu-item title="Public Site" icon="o-globe-alt" link="/" />
+                </x-menu>
+            </x-slot:sidebar>
+        @endauth
 
         {{-- The `$slot` goes here --}}
         <x-slot:content>
@@ -75,28 +77,30 @@
     <x-toast />
 
     {{-- FAB Quick Create --}}
-    <div class="fixed bottom-6 right-6 z-50" x-data="{ open: false }">
-        <div x-show="open" x-transition class="mb-2 menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li>
-                <a href="{{ route('admin.posts.create') }}" wire:navigate class="flex items-center gap-2">
-                    <x-icon name="o-document-plus" />
-                    <span>{{ __('cms.new_post') }}</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('admin.pages.create') }}" wire:navigate class="flex items-center gap-2">
-                    <x-icon name="o-document-text" />
-                    <span>{{ __('cms.new_page') }}</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('admin.categories.create') }}" wire:navigate class="flex items-center gap-2">
-                    <x-icon name="o-folder-plus" />
-                    <span>{{ __('cms.new_category') }}</span>
-                </a>
-            </li>
+    @auth
+        <div class="fixed bottom-6 right-6 z-50" x-data="{ open: false }">
+            <div x-show="open" x-transition class="mb-2 menu p-2 shadow bg-base-100 rounded-box w-52">
+                <li>
+                    <a href="{{ route('admin.posts.create') }}" wire:navigate class="flex items-center gap-2">
+                        <x-icon name="o-document-plus" />
+                        <span>{{ __('cms.new_post') }}</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.pages.create') }}" wire:navigate class="flex items-center gap-2">
+                        <x-icon name="o-document-text" />
+                        <span>{{ __('cms.new_page') }}</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.categories.create') }}" wire:navigate class="flex items-center gap-2">
+                        <x-icon name="o-folder-plus" />
+                        <span>{{ __('cms.new_category') }}</span>
+                    </a>
+                </li>
+            </div>
+            <x-button icon="o-plus" class="btn-circle btn-lg btn-primary shadow-lg" @click="open = !open" />
         </div>
-        <x-button icon="o-plus" class="btn-circle btn-lg btn-primary shadow-lg" @click="open = !open" />
-    </div>
+    @endauth
 </body>
 </html>
